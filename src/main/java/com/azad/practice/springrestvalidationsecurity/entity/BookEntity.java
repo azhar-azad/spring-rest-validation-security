f0056d9b5d31ecf20a94f83d7b95bc5088b8606d
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-03-02 22:47:42 +0600

    endpoints tested and bug fixed

--- src/main/java/com/azad/practice/springrestvalidationsecurity/entity/BookEntity.java
+++ src/main/java/com/azad/practice/springrestvalidationsecurity/entity/BookEntity.java
@@ -3,10 +3,14 @@ package com.azad.practice.springrestvalidationsecurity.entity;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class BookEntity {
 
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String name;
 	private String author;
