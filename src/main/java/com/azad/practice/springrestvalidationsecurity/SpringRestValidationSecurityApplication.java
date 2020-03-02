eabab7fc7b5fb1be1653cde47ff0f2ece4dd39e3
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-03-02 16:53:33 +0600

    start.spring.io project creation commit

---
+++ src/main/java/com/azad/practice/springrestvalidationsecurity/SpringRestValidationSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.azad.practice.springrestvalidationsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestValidationSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestValidationSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azad/practice/springrestvalidationsecurity/SpringRestValidationSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.practice.springrestvalidationsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestValidationSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
